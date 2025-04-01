d48ff1a4bef8b7ae88c07ae596ade4ab914c284c
desarrollo <Marta Quiros>
2025-04-01 17:47:48 +0200

    prueba

--- src/api/class/controllers/class.ts
+++ src/api/class/controllers/class.ts
@@ -7,46 +7,43 @@ import { factories } from "@strapi/strapi";
 export default factories.createCoreController("api::class.class", () => ({
   async addTeacherClass(ctx) {
     try {
-      const { idTeacher, idClass } = ctx.request.body;
+      const { Name, id } = ctx.request.body;
 
-      const classUpdate = await strapi
-        .documents("api::class.class")
-        .findOne({ documentId: idClass });
-      if (!classUpdate) {
-        return ctx.notFound(`Id  ${idClass} of class not found`);
+      const classRecord = await strapi.db.query("api::class.class").findOne({
+        where: { id: id },
+      });
+      if (!classRecord) {
+        return ctx.notFound(`Id  ${id} of class not found`);
       }
-      console.log("classUpdate->", classUpdate);
 
-      const teacher = await strapi
-        .documents("api::teacher.teacher")
-        .findOne({ documentId: idTeacher });
+      const teacher = await strapi.db.query("api::teacher.teacher").findOne({
+        where: { Name: Name },
+      });
       if (!teacher) {
-        return ctx.notFound(`Teacher ${idTeacher} does not exist`);
+        return ctx.notFound(`Teacher ${Name} does not exist`);
       }
-      console.log("teacher->", teacher);
-
-      const teachers = await strapi.documents("api::teacher.teacher").findMany({
+      const teachers = await strapi.db.query("api::teacher.teacher").findMany({
         where: {
           classes: {
-            id: classUpdate.id,
+            id: classRecord.id,
           },
         },
         populate: {
           classes: true,
         },
       });
-      console.log("teachers->", teachers);
       const updatedTeachers = [...teachers, { id: teacher.id }];
 
-      // Paso 3: Actualizar la clase con la lista de profesores actualizada
       await strapi.db.query("api::class.class").update({
-        where: { id: classUpdate.id },
-        data: { teachers: updatedTeachers.map((t) => t.id) }, // Aquí asignamos solo los ids de los profesores
+        where: { id: classRecord.id },
+        data: { teachers: updatedTeachers.map((t) => t.id) },
       });
 
       return {
         ok: true,
-        mensaje: `Teacher ${idTeacher} joined correctly`,
+        mensaje: `Teacher ${Name} joined correctly`,
+        clase: classRecord.Name,
+        profesor: teacher.Name,
       };
     } catch (error) {
       return ctx.badRequest("Error joining teacher", { error: error.message });
